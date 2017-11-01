b877a381bfa67c01e2b3cdf75ff174d6d5f379a8
a <a@a.com>
2017-11-02 03:21:07 +0530

    cross domain

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const app = express()
 
 let topItunes = []
 
+var allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain)
+
 axios.get('http://lyrics.wikia.com/wiki/LyricWiki')
      .then((response)=>{
        let $;
